208efb4b78de7ce96e8168a5ca26a26cde8b23f4
Aon <aeolaojo@gmail.com>
2025-09-10 10:10:16 +0100

    Organize .js file

--- javascript.js
+++ javascript.js
@@ -1,63 +1,39 @@
-// function playGame() {
-//   let humanScore = 0,
-//     computerScore = 0,
-//     numberOfRounds = 0;
-
-//   function playRound() {
-//     let humanChoice = getHumanChoice(),
-//     computerChoice = getComputerChoice();
-
-//     numberOfRounds += 1;
-
-//     if (humanChoice === computerChoice) {
-//       console.log("It's a Tie!");
-//     } else if (
-//       (humanChoice === "rock" && computerChoice === "scissors") ||
-//       (humanChoice === "paper" && computerChoice === "rock") ||
-//       (humanChoice === "scissors" && computerChoice === "paper")
-//     ) {
-//       console.log(
-//         `You win! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}!`
-//       );
-//       humanScore += 1;
-//     } else {
-//       console.log(
-//         `You lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}!`
-//       );
-//       computerScore += 1;
-//     }
-
-//     console.log(
-//       `ScoreBoard => ${humanScore} : ${computerScore} (Round ${numberOfRounds})`
-//     );
-//   }
-// }
-
+// DOM ELEMENTS
 const rock = document.querySelector("#rock"),
   paper = document.querySelector("#paper"),
   scissors = document.querySelector("#scissors"),
-  playGame = document.querySelector("#playGame"),
+  playGamebtn = document.querySelector("#playGame"),
   main = document.querySelector("main"),
   scoreboard = document.createElement("div");
 
-let gameStarted = false;
+// GAME STATE
+let gameStarted = false,
+  humanChoice = null,
+  computerScore = 0,
+  humanScore = 0;
 
+// PLAYER INPUT
 function getHumanChoice(choice) {
   if (gameStarted === true) {
-    let humanChoice = choice;
-    return humanChoice;
+    humanChoice = choice;
+    humanChoiceMade = true;
   } else {
     alert("Game hasn't started!");
   }
 }
 
+// COMPUTER INPUT
 function getComputerChoice() {
-  let choice = ["rock", "paper", "scissors"];
-  let computerChoice = choice[Math.floor(Math.random() * choice.length)];
-  alert(`I choose ${computerChoice}`);
-  return computerChoice;
+  if (humanChoiceMade === true) {
+    let choice = ["rock", "paper", "scissors"];
+    let computerChoice = choice[Math.floor(Math.random() * choice.length)];
+    return computerChoice;
+  }
 }
 
-rock.addEventListener("click", () => getHumanChoice(rock));
-paper.addEventListener("click", () => getHumanChoice(paper));
-scissors.addEventListener("click", () => getHumanChoice(scissors));
+
+// Add event listeners to buttons
+rock.addEventListener("click", () => getHumanChoice("rock"));
+paper.addEventListener("click", () => getHumanChoice("paper"));
+scissors.addEventListener("click", () => getHumanChoice("scissors"));
+playGamebtn.addEventListener("click", () => playGame());
